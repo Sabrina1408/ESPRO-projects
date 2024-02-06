e4c43b58d31acfd5818a5a408aea3f0b4f33e04b
Sabrina Alves <sabrina.a.brito@gmail.com>
2024-02-05 21:59:46 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+
+<h1>👩🏽‍💻 Site de redirecionamento de link para projetos do ESPRO</h1>
+<h2>🎯 Links e 🎨 Figma</h2>
+<ul>
+  <li>
+    <a href="#" target="_blank">Link do site</a>
+  </li>
+  <li>
+    <a href="https://sabrinaalvesbrito.com.br" target="_blank">Meu portfólio</a>
+  </li>
+  <li>
+    <a href="https://www.figma.com/file/Q4xGFFKOGgKoGaDwC7Tm3l/Untitled?type=design&node-id=0%3A1&mode=design&t=ZtYAx7mRBn7u9lUb-1" target="_blank">figma-site</a>
+  </li>
+</ul>
+<h2>👽 Status - Fazendo</h2>
+<h2>🧐 Aprendizado</h2>
+<ul>
+  <li>Bootstrap.</li>
+</ul>
+<h2>👀 Problemas/Ideias/Fazer</h2>
+<h3>Problemas</h3>
+<ul>
+  <li>✔️ Todos resolvidos.</li>
+</ul>
+<h3>Ideias/Fazer</h3>
+<ul>
+  <li>🕐 Deixar de um jeito fácil pro instrutor editar.</li>
+  <li>🕐 Passar para react.</li>
+</ul>
+<!-- ❌ ✔️ 🕐 -->
---
+++ src/components/scripts/menu.js
@@ -0,0 +1,43 @@
+let btnMobile = document.querySelector("#btnMobile");
+let btnMobileLiA = document.querySelectorAll(".btn-mobile-li-a");
+let header = document.querySelector("#header");
+let body = document.querySelector("#body");
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("click", fixed);
+
+function fixed () {
+    const active = nav.classList.contains("active");
+    if(active){
+      body.style.position = "fixed";
+    } else {
+      body.style.position = "static";
+    }
+}
+function toggleMenu(event) {
+    let nav = document.querySelector("#navHeader");
+    
+    nav.classList.toggle("active");
+    const active = nav.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", active);
+    if(active){
+        event.currentTarget.setAttribute("aria-label", "Fechar menu");
+        arrayLiAs = Array.from(btnMobileLiA);
+        arrayLiAs.forEach(liAs => {
+            liAs.addEventListener("click", toggleMenu);
+        });
+        //header.style.position = "fixed";
+        //header.style.width = "100%";
+        /* body.style.overflow = "hidden"; */
+        //body.style.position = "fixed";
+    } 
+    /* if(btnMobile.style.display == "block" && active == true) {
+        body.style.position = "fixed";
+    } */
+    else {
+        event.currentTarget.setAttribute("aria-label", "Abrir menu");
+        //header.style.position = "";
+        /* body.style.overflowY = "scroll"; */
+        body.style.position = "static";
+    }
+}
